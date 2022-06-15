b37eadacd3c9c7e3ac627bdacbde2989efb87c59
Thomas Liu <liuxk99@gmail.com>
2022-06-15 14:34:39 +0800

    [Dev<#F-feature>]: Add coroutines support.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,28 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import java.util.*
+
+//
+// Add VM options: '-Dkotlinx.coroutines.debug' for debug coroutines
+//  in Run/Debug Configuration
+//
+fun main(args: Array<String>) = runBlocking {
+    println("${Date()}: =>" + Thread.currentThread().name)
+
+    launch {
+        println("${Date()}: =>" + Thread.currentThread().name)
+        delay (100L)
+        println("${Date()}: <-" + Thread.currentThread().name)
+    }
+
+    println("${Date()}: => Thread.sleep()")
+    Thread.sleep(1000L)
+    println("${Date()}: <- Thread.sleep()")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    println("${Date()}: Program arguments: ${args.joinToString()}")
+
+    println("${Date()}: <-" + Thread.currentThread().name)
 }
